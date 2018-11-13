6a079cc18dd7a649136cbc750516dbf29a4cdca9
Brandyn <bbushong@student.neumont.edu>
2018-11-13 11:56:33 -0700

    started up
    added index.js, views file and public file

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var pug = require('pug');
+var path = require('path');
+
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname+'/views');
+app.use(express.static(path.join(__dirname+'/public')));
+
+app.get('/', function(req, res){
+  res.render('index', {
+    "title":"Cameron's p cute"
+  });
+});
+
+app.listen(3000);
