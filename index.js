3464babb6ccb7e93676093da4bc371a73c9e8158
Cameron <CLofton@student.neumont.edu>
2018-11-19 16:30:58 -0700

    Added the menu pages, and a JSON file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var pug = require('pug');
 var path = require('path');
+//var jsonStuff = require('./importantFiles/menuItems.json');
 
 var app = express();
 
@@ -11,6 +12,24 @@ app.use(express.static(path.join(__dirname+'/public')));
 app.get('/', function(req, res){
   res.render('index', {
     "title":"Cameron's p cute"
+  });
+});
+
+app.get('/burgers', function(req, res){
+  res.render('index', {
+    "title":"Welcome to the burgers page."
+  });
+});
+
+app.get('/sides', function(req, res){
+  res.render('index', {
+    "title":"Welcome to the sides page."
+  });
+});
+
+app.get('/desserts', function(req, res){
+  res.render('index', {
+    "title":"Welcome to the desserts page."
   });
 });
 
