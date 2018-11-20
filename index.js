08e501abd0f86188dd683c54befe459b5af8c967
Brandyn <bbushong@student.neumont.edu>
2018-11-19 19:33:09 -0700

    finished layout

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var pug = require('pug');
 var path = require('path');
+var config = require('./config.json');
 //var jsonStuff = require('./importantFiles/menuItems.json');
 
 var app = express();
+app.use(express.static('images'));
 
 app.set('view engine', 'pug');
 app.set('views', __dirname+'/views');
@@ -11,31 +13,43 @@ app.use(express.static(path.join(__dirname+'/public')));
 
 app.get('/', function(req, res){
   res.render('index', {
-    "title":"[B]urgers"
+    "title":"[B]urgers",
+    "config": config
   });
 });
 
 app.get('/directions', function(req, res){
   res.render('dir', {
-    "title":"Directions"
+    "title":"Directions",
+    "config": config
+  });
+});
+
+app.get('/menu', function(req, res){
+  res.render('menu',{
+    "title":"Menu",
+    "config": config
   });
 });
 
 app.get('/burgers', function(req, res){
   res.render('index', {
-    "title":"Welcome to the burgers page."
+    "title":"Welcome to the burgers page.",
+    "config": config
   });
 });
 
 app.get('/sides', function(req, res){
   res.render('index', {
-    "title":"Welcome to the sides page."
+    "title":"Welcome to the sides page.",
+    "config": config
   });
 });
 
 app.get('/desserts', function(req, res){
   res.render('index', {
-    "title":"Welcome to the desserts page."
+    "title":"Welcome to the desserts page.",
+    "config": config
   });
 });
 
